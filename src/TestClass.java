8115a43f80e9b2d1d7fbf69769ef0b88e19b407a
Eric R <ryttingeric@gmail.com>
2022-10-06 20:06:48 -0600

    Added class that uses Result tracker to call sorts and display the profiling data.

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
+/**
+ * Main class that controls and prints out profiling data for each sorting algorithm.
+ * @author Eric R
+ */
 public class TestClass {
+    /**
+     * Uses ArrayGenerator to generate data to sort.
+     * Calls each sort method to sort the generated data then
+     * uses ResultTracker to keep track of profiling data.
+     */
     public static void main(String[] args) {
-        
+        //TODO add other algorithms and array sizes.
+        ArrayList<Integer[]> ints = ArrayGenerator.generatePermutation(3);
+        ResultTracker tracker = new ResultTracker();
+        for (Integer[] i : ints) {
+            Result currentResult = new Result(Arrays.copyOf(i,i.length),Heap.sort(i));
+            System.out.print(currentResult);
+
+            tracker.checkBestOrWorst(currentResult);
+            tracker.addToAverage(currentResult.getCompares());
+        }
+        tracker.print();
     }
 }
